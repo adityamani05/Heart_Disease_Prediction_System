3bebf53bc1c9b2fabc28d274cf56b758b045f017
aditya mani <tripathiaditya05@gmail.com>
2020-04-09 20:48:03 +0530

    First Commit

---
+++ hdps/forms.py
@@ -0,0 +1,75 @@
+from django.contrib.auth  import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib import auth
+from hdps.models import Profile
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length = 256)
+    last_name = forms.CharField(max_length = 256)
+    class Meta:
+        fields = ('username','first_name','last_name','email','password1','password2')
+        model = get_user_model()
+
+        def __init__(self,*args,**kwargs):
+            super().__init__(*args,**kwargs)
+            self.fields['username'].label = 'USER NAME'
+            self.fields['first_name'].label = 'FIRST NAME'
+            self.fields['last_name'].label = 'LAST NAME'
+            self.fields['email'].label = 'EMAIL'
+            self.fields['password1'].label = 'PASSWORD'
+            self.fields['password2'].label = 'CONFORM PASSWORD'
+            
+class ProfileModelForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        # fields = '__all__
+        exclude = ['user']
+
+class UserReportForm(forms.Form):
+    SEX_CHOICES = [
+        (1,'Male'),
+        (0,'Female'),
+        (0,'Others')
+    ]
+    CHEST_PAIN_CHOICES = [
+        (1,'Typical Angina'),
+        (2,'Atypical Angina'),
+        (3,'Non-Anignal Pain'),
+        (4,'Asymptotic')
+    ]
+    BLOOD_SUGAR_CHOICES = [
+        (0,'Blood Sugar < 120mg/dl'),
+        (1,'Blood Sugar > 120mg/dl')
+    ]
+    ECG_CHOICES = [
+        (0,'Normal'),
+        (1,'Having ST-T Wave Abromality'),
+        (2,'Left Ventricular Hyperthrophy')
+    ]
+    EXERCISE_ANGINA_CHOICES = [
+        (1,'Yes'),
+        (0,'No')
+    ]
+    PEAK_ST_SEGMENT_CHOICES = [
+        (1,'Upsloping'),
+        (2,'Flat'),
+        (3,'Downsloping')
+    ]
+    THAL_CHOICES=[
+        (3,'Normal'),
+        (6,'Fixed Defect'),
+        (7,'Reversible Defect')
+    ]
+    age = forms.IntegerField()
+    sex = forms.ChoiceField(choices= SEX_CHOICES)
+    cp = forms.ChoiceField(choices= CHEST_PAIN_CHOICES)
+    # restbps = forms.IntegerField()
+    chol = forms.IntegerField()
+    fbs = forms.ChoiceField(choices=BLOOD_SUGAR_CHOICES)
+    # ecg = forms.IntegerField(choices=ECG_CHOICES)
+    # heart_rate = forms.IntegerField()
+    ex_in_angina = forms.ChoiceField(choices = EXERCISE_ANGINA_CHOICES)
+    st_depression_in_exercise = forms.FloatField()
+    peak_st_segment = forms.ChoiceField(choices=PEAK_ST_SEGMENT_CHOICES)
+    vessels_by_flourosopy = forms.FloatField()
+    thal = forms.ChoiceField(choices=THAL_CHOICES)
---
+++ hdps/views.py
@@ -0,0 +1,136 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect,HttpResponse
+from django.urls import reverse_lazy,reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic.detail import DetailView,SingleObjectMixin
+from django.views.generic.list import ListView
+from hdps import forms
+import openpyxl
+from .models import Profile
+from .forms import ProfileModelForm,SignUpForm,UserReportForm
+from .models import UserReport
+from hdps.apps import HdpsConfig
+from sklearn.preprocessing import StandardScaler as ss
+import numpy as np
+from django.contrib import messages
+
+# Create your views here.
+class SignUp(CreateView):
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy('hdps:login')
+    template_name = 'signup.html'
+
+class ProfileForm(LoginRequiredMixin,CreateView):
+    form_class = forms.ProfileModelForm
+    template_name = 'profile_form.html'
+
+    def form_valid(self, form):
+        fields = ['age','birth_date','phone_no','address','profile_pic']
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('hdps:profile-detail',kwargs={'pk':self.object.user.id})
+
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = 'profile_detail.html'
+
+    def get_object(self):
+        try:
+            object = super().get_object()
+        except:
+            object = None
+            return object
+        return object
+
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileModelForm
+    template_name = 'profile_form.html'
+
+    def get_object(self):
+        id_ = self.request.user.id
+        return super().get_object()
+
+    def form_valid(self, form):
+        fields = ['age','birth_date','phone_no','address','profile_pic']
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('hdps:profile-detail',kwargs={'pk':self.object.user.id})
+
+#############################################################################################
+
+def userReportView(request):
+    if request.method == 'POST':
+        form = UserReportForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            age = form.cleaned_data['age']
+            sex = form.cleaned_data['sex']
+            cp = form.cleaned_data['cp']
+            chol = form.cleaned_data['chol']
+            fbs = form.cleaned_data['fbs']
+            ex_in_angina = form.cleaned_data['ex_in_angina']
+            st_depression_in_exercise = form.cleaned_data['st_depression_in_exercise']
+            peak_st_segment = form.cleaned_data['peak_st_segment']
+            vessels_by_flourosopy = form.cleaned_data['vessels_by_flourosopy']
+            thal = form.cleaned_data['thal']
+
+            excel_file = request.FILES["excel_file"]
+            wb = openpyxl.load_workbook(excel_file)
+            worksheet = wb.active
+            restbps = worksheet.cell(row=2,column=1).value
+            ecg =  worksheet.cell(row=2,column=2).value
+            heart_rate =  worksheet.cell(row=2,column=3).value
+            u = UserReport(user=user,age=age,sex=sex,cp=cp,restbps=restbps,chol=chol,fbs=fbs,ecg=ecg,heart_rate=heart_rate,ex_in_angina=ex_in_angina,st_depression_in_exercise=st_depression_in_exercise,peak_st_segment=peak_st_segment,vessels_by_flourosopy=vessels_by_flourosopy,thal=thal)
+            u.save()
+            return HttpResponseRedirect(reverse('hdps:heart_prediction'))
+    else:
+        form =UserReportForm()
+    return render(request,'report_form.html',{'form':form})
+############################################################################################
+def home(request):
+    return render(request,'home.html')
+##########################################################################################
+def predict(request):
+    data = UserReport.objects.filter(user=request.user).latest('date_time')
+    print(data.age)
+    reportData = np.array([data.age,data.sex,data.cp,data.restbps,data.chol,data.fbs,data.ecg,data.heart_rate,data.ex_in_angina,data.st_depression_in_exercise,data.peak_st_segment,data.vessels_by_flourosopy,data.thal])
+    reportData = reportData.reshape(1,-1)
+    sc = ss()
+    trained_data = sc.fit_transform(HdpsConfig.trained_data)
+    reportData = sc.transform(reportData)
+    result = HdpsConfig.classifier.predict(reportData)
+    if data.sex == 1:
+        sex = 'MALE'
+    else:
+        sex= 'FEMALE'
+    if data.ecg == 0:
+        ecg_val = 'Normal'
+    elif data.ecg == 1:
+        ecg_val = 'ST-T Wave Abnormality'
+    else:
+        ecg_val = 'Left Ventricular Hyperthrophy'
+
+    try:
+        profile = Profile.objects.get(user = request.user)
+    except:
+        profile = None
+    return render(request,'prediction_report.html',{'report_data':data,'result':result,'sex':sex,'profile':profile,'ecg_val':ecg_val})
+############################################################################################
+
+def getAllReports(request):
+    all_reports = None
+    try:
+        all_reports = UserReport.objects.filter(user=request.user)
+    except Exception as e:
+        messages.add_message(request, messages.INFO, 'No Reports found!!!.')
+    print(all_reports)
+    return render(request,'user_all_reports.html',{'all_reports':all_reports})
